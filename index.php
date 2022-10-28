8ce79efc9bdced328bf53c82f9619f10cbbd10fe
trileeee <89209875+trileeee@users.noreply.github.com>
2022-10-29 02:07:07 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+ dsds
+ <meta charset="UTF-8">
+      </head>
+<body>
+    <form name="btform1" action="..." method="...">
+        <fieldset>
+            <legend>Thông tin tài khoản</legend>
+            <label for="txbTentaikhoan">Tên tài khoản</label><br>
+            <input type="text" name="Username"id="txbTentaikhoan" /><br>
+            
+            <label for="txbMatkhau">Password</label><br>
+            <input type="password" name="Password" id="txbMatkhau" value="....................."/>
+           
+            <input type="submit" value="Đăng nhập">
+        </fieldset>
+      
+        
+    </form> 
+</body>   
+</html>
