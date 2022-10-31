7e0178ff8a20d0bd3567386477c3e8e350494ba8
trileeee <89209875+trileeee@users.noreply.github.com>
2022-10-31 18:21:36 +0700

    add Config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "ltwebdatabase";
+
+    //Create connection 
+    $conn = mysqli_connect ($servername, $username, $password, $dbname);
+    mysqli_set_charset($conn, 'UTF8');
+    //Check connection
+    if (!$conn) {
+        die ("Connection failed: ". mysqli_connect_error());
+    }  
+   
+    
+?>
