8a07a3985bdd5e8c06404480fa601493d86a02c3
linhlinh131002 <117087416+linhlinh131002@users.noreply.github.com>
2022-11-01 21:04:47 +0700

    Update menutop.php

--- layout/menutop.php
+++ layout/menutop.php
@@ -1,5 +1,6 @@
 <nav class="container-fluid menu-top">
-<a class="col-md-8" href="?request=home">Home</a>
+<a class="col-md-6" href="?request=home">Home</a>
 <a class="col-md-2" href="?request=login">Login</a>
 <a class="col-md-2" href="?request=register">Register</a>
+<a class ="col-md-2" href = "? request = logout">logout < / a  >
 </nav>
